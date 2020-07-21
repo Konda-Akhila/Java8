6105cb573cf40b2d52afdeeec038289d1f57fe13
Konda-Akhila <akhilaappu68@gmail.com>
2020-07-21 19:51:07 +0530

    First commit

---
+++ Average/src/main/java/average/Average/App.java
@@ -0,0 +1,13 @@
+package average.Average;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
