3387af2bfd314b260f664243b18108ad8c6f1fd0
Konda-Akhila <akhilaappu68@gmail.com>
2020-07-21 20:01:43 +0530

    Third commit

---
+++ Palindrome1/src/main/java/palindrome1/Palindrome1/Palindrome1.java
@@ -0,0 +1,32 @@
+package palindrome1.Palindrome1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+interface MyInterface {
+    List<String> palindrome(List<String> list);
+}
+
+
+public class Palindrome1 {
+	public static void main(String args[]) {
+		MyInterface ref = (list) -> {
+            List<String> l=new ArrayList<String>();
+            for(String str:list) {
+            	String result = "";
+            	for (int i = str.length()-1; i >= 0 ; i--) {
+            		result += str.charAt(i);
+            	}
+            	if(str.equals(result)) {
+            		l.add(result);
+            	}
+            }
+            return l;
+        };
+        List<String> strList = Arrays.asList("ababa", "abca", "mnlc","bjh", "vcvc", "madam", "aaaml");
+        List<String> str=ref.palindrome(strList);
+        System.out.println(str);
+	}
+
+}
